8c4f25b67bb8c781c6c7631ae7a9dfddaecee7d1
skidsirongenesisdev@gmail.com <skidsirongenesisdev@gmail.com>
2016-05-25 19:56:03 +0100

    Added Flint & Steel Functionality to Fyrestone Sword

--- src/main/java/com/skidsdev/fyrestone/item/ItemFyrestoneSword.java
+++ src/main/java/com/skidsdev/fyrestone/item/ItemFyrestoneSword.java
@@ -2,8 +2,16 @@ package com.skidsdev.fyrestone.item;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemSword;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 public class ItemFyrestoneSword extends ItemSword
 {
@@ -21,4 +29,25 @@ public class ItemFyrestoneSword extends ItemSword
 		
 		return false;
 	}
+	
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        pos = pos.offset(facing);
+
+        if (!playerIn.canPlayerEdit(pos, facing, stack))
+        {
+            return EnumActionResult.FAIL;
+        }
+        else
+        {
+            if (worldIn.isAirBlock(pos))
+            {
+                worldIn.playSound(playerIn, pos, SoundEvents.item_flintandsteel_use, SoundCategory.BLOCKS, 1.0F, itemRand.nextFloat() * 0.4F + 0.8F);
+                worldIn.setBlockState(pos, Blocks.fire.getDefaultState(), 11);
+            }
+
+            stack.damageItem(2, playerIn);
+            return EnumActionResult.SUCCESS;
+        }
+    }
 }
