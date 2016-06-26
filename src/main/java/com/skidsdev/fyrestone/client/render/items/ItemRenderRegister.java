a3c4c267233e00aada1f94e24f54be87ec4d0342
skidsirongenesisdev@gmail.com <skidsirongenesisdev@gmail.com>
2016-06-26 15:45:18 +0100

    Cleaned up item model register code

--- src/main/java/com/skidsdev/fyrestone/client/render/items/ItemRenderRegister.java
+++ src/main/java/com/skidsdev/fyrestone/client/render/items/ItemRenderRegister.java
@@ -1,5 +1,7 @@
 package com.skidsdev.fyrestone.client.render.items;
 
+import com.skidsdev.fyrestone.item.ItemBaseShard;
+import com.skidsdev.fyrestone.item.ItemBaseShard.EnumShardType;
 import com.skidsdev.fyrestone.item.ItemRegister;
 
 import net.minecraft.client.Minecraft;
@@ -11,15 +13,22 @@ public class ItemRenderRegister
 {
 	public static void registerItemRenderer()
 	{
-		reg(ItemRegister.itemShard);
 		reg(ItemRegister.itemFyrestoneIngot);
 		reg(ItemRegister.itemFyrestoneSword);
 		reg(ItemRegister.itemFyrestoneCatalyst);
+		
+		for (EnumShardType type : EnumShardType.values())
+		{
+			String itemModelName = ItemRegister.itemShard.getRegistryName().toString() + type.getName();
+			reg(ItemRegister.itemShard, itemModelName, type.ordinal());
+		}
 	}
 	
-	public static void reg(Item item)
+	private static void reg(Item item) { reg(item, 0); }
+	private static void reg(Item item, int meta) { reg(item, item.getRegistryName().toString(), meta); }
+	private static void reg(Item item, String regName, int meta)
 	{
-		ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation(item.getRegistryName().toString(), "inventory");
-		ModelLoader.setCustomModelResourceLocation(item, 0, itemModelResourceLocation);
+		ModelResourceLocation itemModelResourceLocation = new ModelResourceLocation(regName, "inventory");
+		ModelLoader.setCustomModelResourceLocation(item, meta, itemModelResourceLocation);
 	}
 }
