a7696b334bca55a077f32552dc5cabf9dc8932ea
skidsirongenesisdev@gmail.com <skidsirongenesisdev@gmail.com>
2016-06-27 02:08:18 +0100

    Made minor semantic changes

--- src/main/java/com/skidsdev/fyrestone/utils/VersionInfo.java
+++ src/main/java/com/skidsdev/fyrestone/utils/VersionInfo.java
@@ -3,7 +3,7 @@ package com.skidsdev.fyrestone.utils;
 public class VersionInfo
 {
 	public static final String ModId = "fyrestone";
-	public static final String ModName = "Fyrestone Mod";
-	public static final String Version = "@VERSION@";
-	public static final String MCVersion = "@MCVERSION@";
+	public static final String ModName = "Fyrestone";
+	public static final String Version = "0.6.0";
+	public static final String MCVersion = "1.9.4";
 }
